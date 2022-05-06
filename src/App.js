fcc6b5b64542ff77c3b74a9594568d5edadfea01
Krintox <shashanksuggala@gmail.com>
2022-05-06 19:00:10 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+// import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className = "login-form-div">      
+        <div className = "login-page">
+          {/* <div className = "form-div"> */}
+            <div className = "google-btn">
+              <button type="button" class="login-with-google-btn" >
+                Sign in with Google
+              </button>
+            </div>
+
+            <p className = "login-choice">
+              ----------------- or Sign In with your email -----------------
+            </p>
+
+            <div className = "login-form">
+              <form method = "post">
+                <div className = "credentials">
+                  <p>Email Address</p>
+                  <input type = "email-address" placeholder = "Enter your Email" className = "form-input" />
+                  <div className = "password-heading">
+                  <p>Password</p>
+                  </div>
+                  <input type = "password" placeholder = "Enter your password" className = "form-input" />
+                </div>
+                <div className = "submit-btn">
+                  <input type = "submit" value = "Sign In" className = "submit-button" />
+                </div>
+              </form>
+            {/* </div> */}
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+export default function Navbar() {
+
+    return(
+        <nav>
+            <ul classname = "logo"></ul>
+            <ul classname = "nav-text">
+                <button classname = "create-btn">
+                    Create an account
+                </button>
+            </ul>
+        </nav>
+    )
+}
